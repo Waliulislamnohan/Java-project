232de193304cdd94af25cfe7ce7b12f00a0a6870
MD Waliul Islam Nohan <waliulnohan@gmail.com>
2023-09-18 23:55:45 +0600

    update

---
+++ src/main/java/help/karigori/KarigoriApplication.java
@@ -0,0 +1,11 @@
+package help.karigori;
+
+public class KarigoriApplication {
+	private static String my_str = "Hello world";
+
+	public static void main(String[] args) {
+		while(true){
+			System.out.println(my_str);
+		}
+	}
+}
---
+++ src/test/java/help/karigori/KarigoriApplicationTests.java
@@ -0,0 +1,13 @@
+package help.karigori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarigoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
